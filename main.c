f4df7645e490cb7447154172a82ba2f5c46f9be7
Pacome BERNIER <pbernier@e3r13p12.42.fr>
2017-07-11 16:10:29 +0200

    [pbernier] - 2 segfault left

--- main.c
+++ main.c
@@ -6,15 +6,15 @@
 /*   By: rlecart <rlecart@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/15 11:41:03 by rlecart           #+#    #+#             */
-/*   Updated: 2017/07/10 16:24:43 by pbernier         ###   ########.fr       */
+/*   Updated: 2017/07/11 16:07:54 by pbernier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
 #include "includes/ft_printf.h"
-
+//"%hhS, %hhS", 0, L"米米"
 int		main(void)
 {
-	ft_printf("%hhS, %hhS", 0, L"米米");
+	ft_printf("%zo, %zu, %zx, %zX, %zD, %zO", USHRT_MAX, USHRT_MAX, USHRT_MAX, USHRT_MAX, USHRT_MAX, USHRT_MAX);
 	return (0);
 }
--- src/find_specifier.c
+++ src/find_specifier.c
@@ -6,7 +6,7 @@
 /*   By: rlecart <rlecart@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/15 11:41:03 by rlecart           #+#    #+#             */
-/*   Updated: 2017/07/11 15:32:29 by pbernier         ###   ########.fr       */
+/*   Updated: 2017/07/11 16:08:51 by pbernier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,13 +92,13 @@ int		find_specifier(char spec, char *per, void **arg, va_list ap)
 	spec_syn(spec, &per);
 	while (spec_nosyn[i] && spec_nosyn[i] != spec)
 		i++;
-
-	//	printf("%c, %s, %d\n", spec, per, i);
-	//	while(1);
+	//printf("%s\n", per);
 	if (!ft_strstr(per, "jlu") && !ft_strstr(per, "hhlu") &&
-		!ft_strstr(per, "hhS"))
+		!ft_strstr(per, "hhS") && !ft_strstr(per, "zo") &&
+		!ft_strstr(per, "zu") && !ft_strstr(per, "zx") &&
+		!ft_strstr(per, "zX") && !ft_strstr(per, "zlu") &&
+		!ft_strstr(per, "zD") && !ft_strstr(per, "zO"))
 		*arg = tab[ft_flags(per, i)](spec, ap);
-	//while(1);
 	ft_strdel(&per);
 	return (i);
 }
